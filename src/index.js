40d1968e37ef85fe98b05bf5c682c999065ea1d8
Josh Peak <neozenith.dev@gmail.com>
2017-11-15 22:32:22 +1100

    add: Enable React development to webpack build system

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import three from 'three';
-import d3 from 'd3';
+// import three from 'three';
+// import d3 from 'd3';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import bootstrap from 'bootstrap';
+import Hello from './hello';
+
+ReactDOM.render(
+  <Hello name="World" />,
+  document.body
+);
 
 console.log('Hello Josh.');
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// TODO: const stripLoad = require('strip-loader');
+// //Removes console.log for prod builds
+
 module.exports = {
 
-  /////////////////////////////////
+  //// ENTRY ////
   entry: './src/index.js',
 
-  /////////////////////////////////
+  //// OUTPUT ////
   output: {
-    filename: './js/bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
 
-  /////////////////////////////////
+  //// MODULE ////
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: [/\.jsx?$/, /\.es6$/],
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['env', 'react']
           }
         }
       }
     ]
-  }
+  },
 
-  /////////////////////////////////
+  //// RESOLVE ////
+  resolve: {
+    extensions: ['.js', '.jsx', '.es6']
+  },
+
+  //// PLUGINS ////
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ]
 };
